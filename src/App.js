f81a6890c771054ab8efab5cc4fb578e700b85d1
naereloire <naere.loire@gmail.com>
2020-07-21 18:49:43 -0300

    dev inicial rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import './App.css';
-import firebase from './init-firebase.js';
+import "./App.css";
+import React from "react";
+import firebase from "./init-firebase.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Teste } from "./teste.js";
+import { About } from "./sobre.js";
 
 console.log(firebase);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Teste} />
+        <Route path="/sobre" component={About} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
