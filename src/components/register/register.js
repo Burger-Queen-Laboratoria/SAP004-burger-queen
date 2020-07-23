aa82f8255d888cb795ebe43e3b31ac839bd99b4b
naereloire <naere.loire@gmail.com>
2020-07-23 01:58:42 -0300

    style pocisionamemtos de elementos

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { InputEmail, InputPassword } from "../Components.js";
-import { StyleInput, StyleForm } from "../StyleComponents.js";
+import {
+  StyleInput,
+  StyleForm,
+  CheckboxRegister,
+  BtnsRegisterContainer,
+  StyleBtnRegister,
+} from "../StyleComponents.js";
 
 export const Register = () => {
   return (
     <StyleForm>
-      <h1>Preencha</h1>
+      <h1>Preencha os campos abaixo:</h1>
       <StyleInput type="text" name="name" placeholder="Nome e sobrenome" />
       <InputEmail />
       <InputPassword />
-      <input type="checkbox" id="hall" name="hall"></input>
-      <label htmlFor="hall">Salão</label>
-      <input type="checkbox" id="kitchen" name="kitchen"></input>
-      <label htmlFor="kitchen">Cozinha</label>
-      <button type="submit">Registrar</button>
-      <button type="button">Voltar</button>
+      <CheckboxRegister>
+        <input type="checkbox" id="hall" name="hall"></input>
+        <label htmlFor="hall">Salão</label>
+        <input type="checkbox" id="kitchen" name="kitchen"></input>
+        <label htmlFor="kitchen">Cozinha</label>
+      </CheckboxRegister>
+      <BtnsRegisterContainer>
+        <StyleBtnRegister type="submit">Registrar</StyleBtnRegister>
+        <StyleBtnRegister type="button">Voltar</StyleBtnRegister>
+      </BtnsRegisterContainer>
     </StyleForm>
   );
 };
