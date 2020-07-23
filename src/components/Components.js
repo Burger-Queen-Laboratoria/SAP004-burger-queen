d6d808036b40e9394e4370d81f282b4bcd1b9170
jessicamelise <jessica.melise@hotmail.com>
2020-07-23 17:38:15 -0300

    refatoração dos componentes para melhor reutilização

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { StyleInput, StyleButton, StyleError } from "./StyleComponents.js";
 
-export const InputEmail = (props) => {
+export const InputComponent = (props) => {
   return (
-    <StyleInput type="e-mail" placeholder="exemplo@exemplo.com" onBlur={props.func}></StyleInput>
+    <StyleInput type={props.type} placeholder={props.text} onBlur={props.func}></StyleInput>
   )
 };
 
-export const InputPassword = (props) => {
+export const BtnBackgroundBlackComponent = (props) => {
   return (
-    <StyleInput type="password" placeholder="senha" onBlur={props.func}></StyleInput>
-  )
-};
-
-export const BtnLogin = (props) => {
-  return (
-    <StyleButton type="submit" onClick={props.func}>Entrar</StyleButton>
-  )
-};
-
-export const BtnLogout = (props) => {
-  return (
-    <button type="submit" onClick={props.func}>Sair</button>
+    <StyleButton type={props.type} onClick={props.func}>{props.name}</StyleButton>
   )
 };
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import {
-  InputEmail,
-  InputPassword,
-  BtnLogin,
+  InputComponent,
+  BtnBackgroundBlackComponent,
   ErrorArea,
 } from "../components/Components.js";
 import { authSignIn } from "../firebase/firebaseFunctions.js";
@@ -49,9 +48,9 @@ export const LoginPage = () => {
       <ImgLogo src={logoImg} alt="logo-burger-queen"></ImgLogo>
       <TitleLogo>Burger Queen</TitleLogo>
       <StyleForm>
-        <InputEmail func={handleInputEmail} />
-        <InputPassword func={handleInputPassword} />
-        <BtnLogin func={handleClick} />
+        <InputComponent type="email" text="exemplo@exemplo.com" func={handleInputEmail} />
+        <InputComponent type="password" text="senha" func={handleInputPassword} />
+        <BtnBackgroundBlackComponent type="submit" name="Entrar" func={handleClick} />
       </StyleForm>
       <ErrorArea err={errorLogin} />
       <NewMember>Funcionário novo?</NewMember>
