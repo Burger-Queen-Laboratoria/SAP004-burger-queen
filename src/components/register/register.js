969e6d002ad5b0a2dfc6be54baa8fc36002f44b1
naereloire <naere.loire@gmail.com>
2020-07-23 01:15:27 -0300

    add checkbox

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { InputEmail, InputPassword } from "../Components.js";
-import { StyleInput } from "../StyleComponents.js";
+import { StyleInput, StyleForm } from "../StyleComponents.js";
 
 export const Register = () => {
   return (
-    <form>
+    <StyleForm>
       <h1>Preencha</h1>
-      <StyleInput type="text" name="name" placeholder="Digite o seu nome" />
+      <StyleInput type="text" name="name" placeholder="Nome e sobrenome" />
       <InputEmail />
       <InputPassword />
+      <input type="checkbox" id="hall" name="hall"></input>
+      <label htmlFor="hall">Salão</label>
+      <input type="checkbox" id="kitchen" name="kitchen"></input>
+      <label htmlFor="kitchen">Cozinha</label>
       <button type="submit">Registrar</button>
       <button type="button">Voltar</button>
-    </form>
+    </StyleForm>
   );
 };
