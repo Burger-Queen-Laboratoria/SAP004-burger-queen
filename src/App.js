dacfb1259f1ceb04f3415a75561dd9b39b9bd359
jessicamelise <jessica.melise@hotmail.com>
2020-08-06 20:18:16 -0300

    criação das rotas privadas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage.js";
 import { Register } from "./pages/Register.js";
 import { LoungePage } from "./pages/LoungePage.js";
 import { KitchenPage } from "./pages/KitchenPage.js";
-import { HistoricArea } from "./pages/HistoricPage.js"
+import { HistoricArea } from "./pages/HistoricPage.js";
+import { fireFuncs } from "./firebase/firebaseFunctions.js";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact={true} component={LoginPage} />
-        <Route path="/register" component={Register} />
-        <Route path="/lounge" component={LoungePage} />
-        <Route path="/kitchen" component={KitchenPage} />
-        <Route path="/historic" component={HistoricArea} />
-      </Switch>
-    </BrowserRouter>
-  );
+  const [userLogged, setUserLogged] = useState();
+
+  useEffect(() => {
+    fireFuncs.getLoggedUser((user) => {
+      user ? 
+      fireFuncs.getCurrentUser(user.uid).then(doc=>setUserLogged(doc.data().sector)) : 
+      setUserLogged()
+    });
+  }, []);
+
+  const redirect = (logged) => {
+    if (logged === "Kitchen") {
+      return (
+        <BrowserRouter>
+          <Redirect to="/kitchen"/>
+          <Switch>
+            <Route path="/kitchen" component={KitchenPage} />
+            <Route path="/historic" component={HistoricArea} />
+          </Switch>
+        </BrowserRouter>
+      );
+    } else if (logged === "Hall") {
+      return (
+        <BrowserRouter>
+          <Redirect to="/lounge"/>
+          <Switch>
+            <Route path="/lounge" component={LoungePage} />
+          </Switch>
+        </BrowserRouter>
+      )
+    } else {
+      return (
+        <BrowserRouter>
+          <Redirect to="/"/>
+          <Switch>
+            <Route path="/" component={LoginPage} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </BrowserRouter>
+      );
+    }
+  }
+
+
+  return redirect(userLogged);
 }
 
 export default App;
