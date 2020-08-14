5ca5b7c939271a2d9dff02179dad58d1295ddfe5
naereloire <naere.loire@gmail.com>
2020-08-14 10:04:56 -0300

    dev inicial de testes

--- src/test/register.test.js
+++ src/test/register.test.js
@@ -1,20 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../App";
-import { shallow, mount } from "./enzyme.js";
+import { shallow, mount, render } from "./enzyme.js";
 import { Register } from "../pages/Register.js";
+
+import { Checkbox } from "../components/Checkbox";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
 
 it("should display an input to fill question", () => {
   const wrapper = shallow(<Register />);
   expect(wrapper.find("InputComponent").length).toBe(3);
 });
 
-it("should display an input to fill question", () => {
-  const wrapper = mount(<Register />);
-  expect(wrapper.find("Checkbox").length).toBe(1);
+describe("Checkbox", () => {
+  it("should display an input to fill question", () => {
+    const wrapper = mount(<Register />);
+    expect(wrapper.find("Checkbox").length).toBe(1);
+  });
+  it("Should render Checkbox without errors", () => {
+    render(<Checkbox onChange={() => {}} defaultValue="default value" />);
+  });
 });
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
+// it("pass a selected value to the onChange handler", () => {
+//   const value = "2";
+//   const options = "cargo";
+//   const onChange = jest.fn();
+//   const wrapper = shallow(<Checkbox options={options} onChange={onChange} />);
+
+//   expect(wrapper).toMatchSnapshot(Checkbox);
+
+//   wrapper.find("Checkbox").simulate("change", {
+//     target: { value },
+//   });
+
+//   expect(onChange).toBeCalledWith(value);
+// });
