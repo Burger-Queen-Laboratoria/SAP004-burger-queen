afdd8ab22de2a8662da44b3df32c45ffb4348014
naereloire <naere.loire@gmail.com>
2020-08-14 10:07:54 -0300

    dev inicial de testes

--- src/test/register.test.js
+++ src/test/register.test.js
@@ -16,15 +16,15 @@ it("should display an input to fill question", () => {
   expect(wrapper.find("InputComponent").length).toBe(3);
 });
 
-describe("Checkbox", () => {
-  it("should display an input to fill question", () => {
-    const wrapper = mount(<Register />);
-    expect(wrapper.find("Checkbox").length).toBe(1);
-  });
-  it("Should render Checkbox without errors", () => {
-    render(<Checkbox onChange={() => {}} defaultValue="default value" />);
-  });
-});
+// describe("Checkbox", () => {
+//   it("should display an input to fill question", () => {
+//     const wrapper = mount(<Register />);
+//     expect(wrapper.find("Checkbox").length).toBe(1);
+//   });
+//   it("Should render Checkbox without errors", () => {
+//     render(<Checkbox onChange={() => {}} defaultValue="default value" />);
+//   });
+// });
 
 // it("pass a selected value to the onChange handler", () => {
 //   const value = "2";
